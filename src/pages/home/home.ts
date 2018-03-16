2d752f16fe10c0e9680c2da68fd39764966be847
Alex Moreno <alex@zukkin.com>
2018-03-15 22:26:45 -0300

    Feito a leitura dos dados por uma api externa e apresentando os dados na view

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {HttpClient} from "@angular/common/http";
+import {Carro} from "../../model/carro";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    public carros: Carro[];
 
-  }
+    constructor(public navCtrl: NavController, private _http: HttpClient) {
+        this._http.get<Carro[]>('http://localhost:8080/api/carro/listaTodos')
+            .subscribe(
+                (carros: Carro[]) => this.carros = carros,
+                error => console.error(error)
+            );
+    }
 
 }
